3210556812b640f4c72fed6b40b0dbeebdd174ae
Leonardo Tononi <easaaa@protonmail.com>
2019-07-07 17:15:48 +0200

    Changed view page

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ $(function () {
   $(document).scroll(function () {
     var $nav = $("#mainNavbar");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-   });
+  });
 });
 
-$(function() {
-// Smooth Scrolling
-$('a[href*="#"]:not([href="#"])').click(function() {
-if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname ==     this.hostname) {
-  var target = $(this.hash);
-  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-  if (target.length) {
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-    return false;
-  }
-}
-});
+$(function () {
+  // Smooth Scrolling
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
 // COPY BTN
@@ -37,61 +37,61 @@ function copyToClipboard(element) {
 
 window.sr = ScrollReveal();
 sr.reveal('.navbar', {
-duration: 3000,
-origin: 'left'
+  duration: 3000,
+  origin: 'left'
 });
 sr.reveal('.title-effect-left', {
-duration: 1000,
-origin: 'left',
-distance: '150px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'left',
+  distance: '150px',
+  viewFactor: 0.2
 });
 sr.reveal('.title-effect-right', {
-duration: 1300,
-origin: 'right',
-distance: '170px',
-viewFactor: 0.2
+  duration: 1300,
+  origin: 'right',
+  distance: '170px',
+  viewFactor: 0.2
 });
 sr.reveal('.title-effect-left2', {
   duration: 1500,
   origin: 'left',
   distance: '190px',
   viewFactor: 0.2
-  });
+});
 window.sr = ScrollReveal();
 sr.reveal('.scrollH', {
-duration: 2000,
-origin: 'top',
-distance: '300px'
+  duration: 2000,
+  origin: 'top',
+  distance: '300px'
 });
 window.sr = ScrollReveal();
 sr.reveal('#about', {
-duration: 1000,
-origin: 'bottom',
-distance: '300px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'bottom',
+  distance: '300px',
+  viewFactor: 0.2
 });
 sr.reveal('#services', {
-duration: 1000,
-origin: 'bottom',
-distance: '300px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'bottom',
+  distance: '300px',
+  viewFactor: 0.2
 });
 sr.reveal('.infoL', {
-duration: 1000,
-origin: 'left',
-distance: '150px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'left',
+  distance: '150px',
+  viewFactor: 0.2
 });
 sr.reveal('.infoR', {
-duration: 1000,
-origin: 'right',
-distance: '150px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'right',
+  distance: '150px',
+  viewFactor: 0.2
 });
 sr.reveal('#portfolio', {
-duration: 1000,
-origin: 'bottom',
-distance: '150px',
-viewFactor: 0.2
+  duration: 1000,
+  origin: 'bottom',
+  distance: '150px',
+  viewFactor: 0.2
 });
